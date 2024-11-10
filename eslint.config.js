47e6d6ca6f0e10fcdda658ae68eab71952324b1f
Chris <chrisbreuer93@gmail.com>
2024-11-10 11:49:10 +0100

    chore: use eslint

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import stacks from '@stacksjs/eslint-config'
+
+export default stacks({
+  stylistic: {
+    indent: 2,
+    quotes: 'single',
+  },
+
+  typescript: true,
+  jsonc: true,
+  yaml: true,
+  ignores: [
+    'fixtures/**',
+  ],
+})
